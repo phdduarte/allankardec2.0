b9ade882bc5cf2d879314b5f443b732fd96b4a32
Pedro Duarte <pedrohlduarte@gmail.com>
2020-12-01 21:57:44 -0300

    Add .next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2727716c2c7570209057";
+/******/ 	var hotCurrentHash = "cea9f9818db82d282389";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
