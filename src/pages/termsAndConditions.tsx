27b7cdb1980f0b94b822f9b447bde3dd28f7aeed
AliferDS <aliferduartesilva@gmail.com>
2020-12-12 17:19:04 -0300

    Change: adicionada pagina termos e condicoes

---
+++ src/pages/termsAndConditions.tsx
@@ -0,0 +1,149 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+import PageTemplate from '../components/templates/pageTemplate'
+import Title from '../components/atoms/title'
+
+const TermsAndConditions: React.FC = () => {
+    return (
+        <div>
+            <PageTemplate titlePage="TERMOS E CONDIÇÕES" mainModel="red-main">
+                <Title label="TERMOS E CONDIÇÕES" />
+                <div className="content-text">
+                    <h2>LICENÇA DO USUÁRIO DO ALLANKARDEC.ONLINE</h2>
+                    <h3>SOBRE ESTA LICENÇA</h3>
+                    <p>
+                        Esta licença de usuário define os termos em que você
+                        pode usar o conteúdo no AllanKardec.online Museu, a
+                        menos que seja indicado de outra forma nos termos de
+                        acesso individuais do conteúdo. Para os propósitos desta
+                        licença de usuário, conteúdo significa qualquer material
+                        (em qualquer formato) que o site disponibilize para você
+                        acessar e imprimir ou baixar no ou através do site
+                        AllanKardec.online. Ao copiar ou usar qualquer conteúdo,
+                        você confirma que aceita as condições desta licença de
+                        usuário.
+                    </p>
+                    <p>
+                        Esta licença de usuário não concede nenhum direito de
+                        usar os nomes, logotipos ou marcas registradas pelo
+                        AllanKardec.online Museu , a menos que expressamente
+                        declarado em contrário.
+                    </p>
+                    <h3>USO PERMITIDO DO CONTEÚDO</h3>
+                    <p>
+                        Você pode imprimir ou baixar qualquer conteúdo apenas
+                        para fins não comerciais, incluindo, mas não limitado a
+                        estudo particular, pesquisa ou ensino e instrução, sob
+                        os seguintes termos:
+                    </p>
+                    <p>
+                        Se o uso permitido do conteúdo envolver qualquer
+                        distribuição, disseminação ou comunicação do conteúdo
+                        (por qualquer meio ou processo) a outra pessoa, você
+                        deve (sempre que possível) reconhecer a fonte do
+                        conteúdo da seguinte forma: “Descrição do documento
+                        utilizado conforme o Título dado ao documento no site
+                        AllanKardec.online”, Crédito da Foto: AllanKardec.online
+                        Museu, vinculando, sempre que possível, o site
+                        AllanKardec.online como a fonte do conteúdo.
+                    </p>
+                    <p>
+                        Outras leis de direitos autorais, proteção de dados e
+                        privacidade podem ainda se aplicar ao conteúdo.
+                    </p>
+                    <h3>USO PROIBIDO DO CONTEÚDO</h3>
+                    <p>
+                        Você não deve usar nenhuma parte do conteúdo para fins
+                        comerciais, exceto se obtiver autorização por escrito.
+                        Para os propósitos desta licença de usuário, fins
+                        comerciais significa qualquer uso do conteúdo que é
+                        primariamente destinado ou direcionado para vantagem
+                        comercial ou compensação monetária. Isto inclui qualquer
+                        uso em ou em qualquer coisa que seja cobrado, está
+                        relacionado com algo que é cobrado ou se destina a gerar
+                        lucro.
+                    </p>
+                    <p>
+                        Para mais informações ou dúvidas sobre essa licença de
+                        usuário, consulte-nos através do email:
+                        allankardec.online@gmail.com
+                    </p>
+                    <h2>ALLANKARDEC.ONLINE USER LICENCE</h2>
+                    <h3>ABOUT THIS LICENCE</h3>
+                    <p>
+                        This user licence sets out the terms on which you may
+                        use the content on AllanKardec.online Museum, unless
+                        otherwise stated in the content's individual terms of
+                        access. For the purposes of this user licence, content
+                        means any material (in any format) that the
+                        AllanKardec.online makes available to you to access and
+                        print or download on or through the AllanKardec.online
+                        website from time to time. By copying or using any
+                        content, you confirm that you accept the conditions of
+                        this user licence.
+                    </p>
+                    <p>
+                        This user licence does not grant any rights to use the
+                        names, logos or trademarks of either the
+                        AllanKardec.online, unless expressly stated otherwise.
+                    </p>
+                    <h3>PERMITTED USE OF THE CONTENT</h3>
+                    <p>
+                        You may print off or download any content only for
+                        non-commercial purposes, including but not limited to
+                        private study, research, or teaching and instruction,
+                        under the following terms:
+                    </p>
+                    <p>
+                        If the permitted use of the content involves any
+                        distribution, dissemination or communication of the
+                        content (by any means or process) to another person, you
+                        must (wherever possible) acknowledge the source of the
+                        content in the following form: “Description of the
+                        document used according to the title given to the
+                        document on the AllanKardec.online website”, Photo:
+                        AllanKardec.online Museum, linking where possible to the
+                        AllanKardec.online site as the source of the content.
+                    </p>
+                    <p>
+                        You may not remove any copyright, trademark or other
+                        proprietary notices including attribution metadata,
+                        credits and notices, that are placed in or near the
+                        text, images or data.
+                    </p>
+                    <p>
+                        Further copyright, data protection and privacy laws may
+                        still apply to content. You must comply with all terms
+                        or restrictions other than copyright (such as trademark,
+                        publicity and privacy rights, or contractual
+                        restrictions) as may be specified in the metadata or as
+                        may otherwise apply to the Content. This licence does
+                        not affect your freedom under fair dealing or any other
+                        copyright or database right exceptions and limitations
+                        that might apply to your use.
+                    </p>
+                    <h3>PROHIBITED USE OF THE CONTENT</h3>
+                    <p>
+                        You must not use any part of the content for commercial
+                        purposes, except as permitted by statutory exceptions
+                        and limitations to copyright. For the purposes of this
+                        user licence, commercial purposes means any use of the
+                        content that is primarily intended for or directed
+                        toward commercial advantage or monetary compensation.
+                        This includes any use on or in anything that is itself
+                        charged for, is connected with something that is charged
+                        for or is intended to make a profit.
+                    </p>
+                    <p>
+                        For further information or If you have any comments or
+                        questions about this user licence, please contact:
+                        allankardec.online@gmail.com
+                    </p>
+                </div>
+            </PageTemplate>
+        </div>
+    )
+}
+
+export default TermsAndConditions
