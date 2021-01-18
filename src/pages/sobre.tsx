5ca4e56d887d1a1f975dd00930c0c3592d866f91
Kelvin Chaves <kelvincs@pop-os.localdomain>
2021-01-18 08:25:48 -0300

    Corrigido erros de merge

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 
 import PageTemplate from '../components/templates/pageTemplate'
 import Title from '../components/atoms/title'
-<<<<<<< HEAD
-=======
 import { fontsize } from '*.jpg'
->>>>>>> origin
 
 const Contacts: React.FC = () => {
     return (
@@ -15,47 +12,6 @@ const Contacts: React.FC = () => {
                 <Title label="Sobre" />
                 <div className="content-text text-justify">
                     <p>
-<<<<<<< HEAD
-                        Os itens que compõe o museu AllanKardec.online são raros, sendo que muitos
-                        são inéditos etotalmente desconhecidos do Movimento Espírita Brasileiro e
-                        do Espiritismo internacional. Oacervo disponibilizado é extremamente
-                        significativo, tanto do ponto de vista acadêmico,quanto como material de
-                        grande riqueza histórica para todos os interessados em estudar e pesquisar
-                        o trabalho do mestre Allan Kardec e do Espiritismo.
-                    </p>
-                    <p>
-                    O museu virtual não vai medir esforços para tornar o acervo digitalizado de
-                    forma aberta parauma ampla variedade de usuários de todo o mundo, com o
-                    intuito de conhecimento, aprendizagem,estudos, ensino e pesquisa. Vamos
-                    procurar digitalizar todo o acervo e disponibilizá-lo, gratuitamente, para o
-                    bem e divulgação do Espiritismo, sem qualquer viés político ou ideológico. 
-                    </p>
-                    <p>
-                    Se você perguntar a algum frequentador assíduo de centro espírita,
-                    provavelmente receberá a seguinte resposta: o Espiritismo é uma doutrina
-                    revelada pelos espíritos superiores a Allan Kardec, que a codificou em cinco
-                    obras: O Livro dos Espíritos, O Livro dos Médiuns, O Evangelho Segundo o
-                    Espiritismo, O Céu e o Inferno  e A Gênese.
-                    </p>
-                    <p>
-                    Para os estudiosos e livres pensadores do Espiritismo ele responderá que as
-                    obras do mestre Kardec são em número muito maior que isso, incluindo aí as
-                    REVISTA ESPÍRITAS - Jornal deEstudos Psicológicos, publicadas pelo mestre
-                    entre 1858 e 1869.
-                    </p>
-                    <p>
-                    O objetivo do site é permitir que todos possam  ter acesso a documentos raros
-                    e obras, praticamente, desconhecidos da quase totalidade do mundo Espírita,
-                    colaborando assim,  para o desenvolvimento da Doutrina Espírita. Além disso, e
-                    talvez o maior objetivo, é propiciar que pesquisadores possam efetuar estudos
-                    mais profundos nos documentos apresentados e tomar conhecimento de como Allan
-                    Kardec conduziu e elaborou seus estudos para a codificação da doutrina.
-                    </p>
-                    <p>
-                    Com certeza, esta iniciativa propiciará que a história do Espiritismo seja
-                    mais conhecida e, quem sabe, reescrita através das novas informações e
-                    conhecimentos trazidos pelos documentos do acervo apresentado.
-=======
                         Os itens que compõe o museu AllanKardec.online são raros, sendo que muitos são inéditos e
                         totalmente desconhecidos do Movimento Espírita Brasileiro e do Espiritismo internacional. O
                         acervo disponibilizado é extremamente significativo, tanto do ponto de vista acadêmico,
@@ -91,7 +47,6 @@ const Contacts: React.FC = () => {
                     Com certeza, esta iniciativa propiciará que a história do Espiritismo seja mais conhecida e,
                     quem sabe, reescrita através das novas informações e conhecimentos trazidos pelos documentos do
                     acervo apresentado.
->>>>>>> origin
                     </p>
                     <p>
                     Assim seja e assim será!
