98bd2abec0b5e1909f8d5a2fba42d84552c39587
Kelvin Chaves <kelvinhenrique.cs@gmail.com>
2021-01-18 21:27:15 -0300

    Adicionando admin

---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { API_BASE_URL } from '../services/document.service'
+
+const Login = () => (
+    <iframe src={`${API_BASE_URL}/admin`} frameBorder="0" style={{ width: '100vw', height: '100vh' }}></iframe>
+)
+
+export default Login
