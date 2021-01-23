0d1fc810929d6a81324da283288f7263fc6738a9
AliferDS <aliferduartesilva@gmail.com>
2021-01-23 16:30:40 -0300

    reestruturando codigo pagina search

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -8,36 +8,42 @@ import ReactPaginate from 'react-paginate'
 
 import { useDocumentPageCount } from '../hooks/useDocumentPageCount'
 
-import { DOCUMENT_TYPE, documentService, API_BASE_URL } from '../services/document.service'
+import {
+    DOCUMENT_TYPE,
+    documentService,
+    API_BASE_URL
+} from '../services/document.service'
 
-const prepareDocument = (document) => ({
+const prepareDocument = document => ({
     ...document,
     file: { url: `${API_BASE_URL}${document.file.url}` },
-    cover: { url: `${API_BASE_URL}${document.cover.url}` },
-
+    cover: { url: `${API_BASE_URL}${document.cover.url}` }
 })
 
 const Search = ({ documents, page, title }) => {
-    const pageCount = useDocumentPageCount({ title_contains: title}, 12);
+    const pageCount = useDocumentPageCount({ title_contains: title }, 12)
 
     const handlePageChange = ({ selected }) => {
-        if (selected === page) return;
+        if (selected === page) return
 
-        const categoryUrl = process.browser ? window.location.href.split('?')[0] : '';
+        const categoryUrl = process.browser
+            ? window.location.href.split('?')[0]
+            : ''
         window.location.href = `${categoryUrl}?title=${title}&page=${selected}`
-        
     }
 
     const pageTitle = `Pesquisando por '${title}'`
-    
+
     return (
         <div>
             <PageTemplate titlePage="Pesquisa" mainModel="red-main">
-
                 <Title label={pageTitle} />
                 <Grid>
                     {documents.map(document => (
-                        <DocumentCard key={document.id} document={prepareDocument(document)}/>   
+                        <DocumentCard
+                            key={document.id}
+                            document={prepareDocument(document)}
+                        />
                     ))}
                 </Grid>
                 <ReactPaginate
@@ -57,9 +63,11 @@ const Search = ({ documents, page, title }) => {
 }
 
 Search.getInitialProps = async ({ query }) => {
-    const { title, page } = query || {};
+    const { title, page } = query || {}
 
-    const documents = await documentService.getDocuments({ title_contains: title })
+    const documents = await documentService.getDocuments({
+        title_contains: title
+    })
 
     return {
         documents,
