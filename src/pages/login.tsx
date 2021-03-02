fbfee8ea2e222718b373630140cfe3c5abc545be
AliferDS <aliferduartesilva@gmail.com>
2021-03-02 15:41:02 -0300

    login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import { API_BASE_URL } from '../services/document.service'
 
-const Login = () => (
-    <iframe src={`${API_BASE_URL}/admin`} frameBorder="0" style={{ width: '100vw', height: '100vh' }}></iframe>
+const Login = props => (
+    <div>
+        <iframe
+            src={props.urlParam}
+            frameBorder="0"
+            style={{ width: '100vw', height: '100vh' }}
+        ></iframe>
+    </div>
 )
+
+export async function getServerSideProps() {
+    const urlParam = `${API_BASE_URL}/admin`
+    return {
+        props: {
+            urlParam: urlParam
+        }
+    }
+}
 
 export default Login
